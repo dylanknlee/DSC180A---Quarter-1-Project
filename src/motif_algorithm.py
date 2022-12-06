bb82d60ac54309e5345e63e6dd510879d2c0cda6
dylanknlee <dylanknlee@gmail.com>
2022-12-05 22:09:19 -0800

    added final updates

--- src/motif_algorithm.py
+++ src/motif_algorithm.py
@@ -8,5 +8,6 @@ def run_algorithm(fp, motif, k):
     
     edge_counts = motif_counting(fp, motif)
     motif_adjacency_matrix = create_motif_adjacency_matrix(edge_counts)
-    cluster_dict = k_clusters(motif_adjacency_matrix, k)    
+    cluster_dict = k_clusters(motif_adjacency_matrix, k)
+    print("The classification of nodes into {0} clusters by counting motif {1} is: {2}".format(k, motif, cluster_dict))    
     return cluster_dict
